2492411028dd15b4fefbf8da334940199178bea3
João Batista <joao16batista@gmail.com>
2016-03-16 21:36:37 +0000

    init

---
+++ tshirt.Data/tshirtContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using tshirt.Data.Models;
+
+namespace tshirt.Data
+{
+    public class TshirtContext : IdentityDbContext<ApplicationUser> {
+        public TshirtContext()
+            : base("tshirtdb", throwIfV1Schema: false)
+        {
+        }
+
+        public static TshirtContext Create() {
+            return new TshirtContext();
+        }
+    }
+}
